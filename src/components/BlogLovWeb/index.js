a37635c32309b8127c59900b05e893107ab16718
Maciej Matuszczak <maciej.matuszczak96@gmail.com>
2018-12-30 15:39:32 +0100

    [EntryComponent] Added EntryComponent and mocks for blogs and entries

--- src/components/BlogLovWeb/index.js
+++ src/components/BlogLovWeb/index.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
+import Entry from '../Entry';
 import './style.css';
 import header from '../../assets/images/weddingheader.png';
+
+import { mock_entries } from '../../mocks';
 
 
 class BlogLovWeb extends Component{
@@ -11,27 +14,9 @@ class BlogLovWeb extends Component{
                     <img src={header} alt="header" className="BlogLovWeb--header-img"/>
                 </div>
                 <div className="BlogLovWeb--content-container">
-                <div className="BlogLovWeb--content">
-                    <h1 className="BlogLovWeb--title">Title</h1>
-                    <p className="BlogLovWeb--text">
-                        Non irure ipsum amet nulla enim veniam laboris cupidatat aliqua enim amet. Officia sit cupidatat ex aliqua sit Lorem laborum incididunt consectetur. Ad do sunt anim enim consequat dolore incididunt ea amet reprehenderit nisi tempor.
-                    </p>
-                    <a className="BlogLovWeb--comment">Skomentuj</a>
-                </div>
-                <div className="BlogLovWeb--content">
-                    <h1 className="BlogLovWeb--title">Title</h1>
-                    <p className="BlogLovWeb--text">
-                        Non irure ipsum amet nulla enim veniam laboris cupidatat aliqua enim amet. Officia sit cupidatat ex aliqua sit Lorem laborum incididunt consectetur. Ad do sunt anim enim consequat dolore incididunt ea amet reprehenderit nisi tempor.
-                    </p>
-                    <a className="BlogLovWeb--comment" >Skomentuj</a>
-                </div>
-                <div className="BlogLovWeb--content">
-                    <h1 className="BlogLovWeb--title">Title</h1>
-                    <p className="BlogLovWeb--text">
-                        Non irure ipsum amet nulla enim veniam laboris cupidatat aliqua enim amet. Officia sit cupidatat ex aliqua sit Lorem laborum incididunt consectetur. Ad do sunt anim enim consequat dolore incididunt ea amet reprehenderit nisi tempor.
-                    </p>
-                    <a className="BlogLovWeb--comment">Skomentuj</a>
-                </div>
+
+                {mock_entries.map(entry => <Entry key={entry.title} entry={entry}/>)}
+
                 <div className="BlogLovWeb--footer">
                 <p>Aliquip commodo id id incididunt ad in eiusmod. Consectetur culpa esse proident adipisicing est 
                     adipisicing. Officia nisi ipsum nostrud adipisicing id nulla eiusmod anim officia ipsum qui 
---
+++ src/mocks.js
@@ -0,0 +1,15 @@
+export const mock_blogs = [
+    {name: 'Test 1', slug: 'test-1', category: 'Design'},
+    {name: 'Test 2', slug: 'test-2', category: 'Design'},
+    {name: 'Test 3', slug: 'test-3', category: 'Travel'},
+    {name: 'Test 4', slug: 'test-4', category: 'Travel'},
+    {name: 'Test 5', slug: 'test-5', category: 'Travel'},
+]
+
+export const mock_entries = [
+    {title: 'Entry 1', content: 'Non irure ipsum amet nulla enim veniam laboris cupidatat aliqua enim amet', createdAt: '2018-12-31T15:21:11.980Z', updatedAt: '2018-12-31T15:21:11.980Z'},
+    {title: 'Entry 2', content: 'Non irure ipsum amet nulla enim veniam laboris cupidatat aliqua enim amet', createdAt: '2018-12-30T11:24:33.980Z', updatedAt: '2018-12-30T11:24:33.980Z'},
+    {title: 'Entry 3', content: 'Non irure ipsum amet nulla enim veniam laboris cupidatat aliqua enim amet', createdAt: '2018-12-29T10:00:00.980Z', updatedAt: '2018-12-29T10:00:00.980'},
+    {title: 'Entry 4', content: 'Non irure ipsum amet nulla enim veniam laboris cupidatat aliqua enim amet', createdAt: '2018-12-28T14:21:45.980Z', updatedAt: '2018-12-28T14:21:45.980Z'},
+    {title: 'Entry 5', content: 'Non irure ipsum amet nulla enim veniam laboris cupidatat aliqua enim amet', createdAt: '2018-12-25T02:30:13.980Z', updatedAt: '2018-12-25T02:30:13.980Z'},
+]
