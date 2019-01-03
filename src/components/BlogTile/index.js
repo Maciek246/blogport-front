2d43c646f70f70afdf25bb4e236550904c734c65
Katie Otto <33523737+kateotto@users.noreply.github.com>
2019-01-03 23:00:51 +0100

    [ListingPostStyle] added style to BlogTile component

--- src/components/BlogTile/index.js
+++ src/components/BlogTile/index.js
@@ -6,8 +6,11 @@ class BlogTile extends Component{
     render(){
         const { blog } = this.props;
         return (
-            <div className="blogtile">
-                <h1><Link to={blog.slug}>{blog.name}</Link></h1>
+            <div className="BlogTile--container">
+                <h1 className="BlogTile--title"><Link to={blog.slug} style={{ textDecoration: 'none', color: '#e5b300' }}>{blog.name}</Link></h1>
+                <div className="BlogTile--author">AUTOR</div>
+                <div className="BlogTile--category">KATEGORIA</div>
+                <div className="BlogTile--description">OPIS</div>
             </div>
         );
     }
