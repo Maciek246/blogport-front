b312d8891f012dfdf524ad8382adfe9a4cd3b0a6
Katie Otto <33523737+kateotto@users.noreply.github.com>
2019-01-03 19:48:28 +0100

    [AddPostPanel] added AddPostPanel component with form to adding a new post

---
+++ src/components/AddPostPanel/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class AddPostPanel extends Component{
+
+
+  render(){
+
+        return (
+            <form>
+                <div className="AddPostPanel--container">
+                    <h1 className="AddPostPanel--title">DODAJ POST</h1>
+                    <br/>
+                    <input type="text" className="AddPostPanel--input" placeholder="Tytuł"/>
+                    <br/>
+                    <textarea className="AddPostPanel--textarea"></textarea>
+                    <br/>
+                    <input type="submit" className="AddPostPanel--submit"/>
+                    <br/>
+                </div>
+          </form>
+    );
+  }
+}
+
+
+export default AddPostPanel;
