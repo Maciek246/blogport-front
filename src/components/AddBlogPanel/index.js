35b658f2c13a4a925ccb4c0ea7dc645f32f5c65e
Artur <>
2019-01-09 00:15:38 +0100

    [creatorNewBlog] Added select list into choose category for new blog

--- src/components/AddBlogPanel/index.js
+++ src/components/AddBlogPanel/index.js
@@ -11,6 +11,20 @@ class AddBlogPanel extends Component {
                     <br />
                     <input type="text" className="AddBlogPanel--input" placeholder="Podaj nazwę Bloga" />
                     <br />
+                    <div>
+                        <label className="AddBlogPanel--label">Wybierz kategorię bloga:
+                        <br />
+                            <select className="AddBlogPanel--select" onChange={this.props.handleOnChange}>
+                                <option value="">All</option>
+                                <option value="Design">Design</option>
+                                <option value="Technology">Technology</option>
+                                <option value="Sport">Sport</option>
+                                <option value="Travel">Travel</option>
+                                <option value="Lifestyle">Lifestyle</option>
+                            </select>
+                        </label>
+                    </div>
+                    <br />
                     <input type="submit" className="AddBlogPanel--submit" />
                     <br />
                 </div>
