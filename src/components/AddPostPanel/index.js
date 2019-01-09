2b034f5160a0b928cb068249cb99feca65f6d9a5
Artur <>
2019-01-09 13:11:30 +0100

    [creatorNewBlog] Fixed header of the form to creating a post blog paying special attention to UX

--- src/components/AddPostPanel/index.js
+++ src/components/AddPostPanel/index.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
 import './style.css';
 
-class AddPostPanel extends Component{
+class AddPostPanel extends Component {
 
 
-  render(){
+    render() {
 
         return (
             <form>
                 <div className="AddPostPanel--container">
-                    <h1 className="AddPostPanel--title">DODAJ POST</h1>
-                    <br/>
-                    <input type="text" className="AddPostPanel--input" placeholder="Tytuł"/>
-                    <br/>
+                    <h1 className="AddPostPanel--title">DODAJ NOWEGO POSTA</h1>
+                    <br />
+                    <input type="text" className="AddPostPanel--input" placeholder="Tytuł" />
+                    <br />
                     <textarea className="AddPostPanel--textarea"></textarea>
-                    <br/>
-                    <input type="submit" className="AddPostPanel--submit"/>
-                    <br/>
+                    <br />
+                    <input type="submit" className="AddPostPanel--submit" />
+                    <br />
                 </div>
-          </form>
-    );
-  }
+            </form>
+        );
+    }
 }
 
 
