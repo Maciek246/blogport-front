b4cb54796b59ceaf0ef624b94e0750b649615baa
Artur <>
2019-01-10 22:30:03 +0100

    [upgradevalidationLogin] Final changes

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,30 +1,85 @@
 import React, { Component } from 'react';
 import './style.css';
 
-class LoginForm extends Component{
+class LoginForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      fields: {},
+      errors: {}
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.submituserLoginForm = this.submituserLoginForm.bind(this);
+
+  };
+
+  handleChange(e) {
+    let fields = this.state.fields;
+    fields[e.target.name] = e.target.value;
+    this.setState({
+      fields
+    });
+
+  }
+
+  submituserLoginForm(e) {
+    e.preventDefault();
+    if (this.validateForm()) {
+      let fields = {};
+      fields["login"] = "";
+      fields["password"] = "";
+      this.setState({ fields: fields });
+      alert("Form submitted");
+    }
+
+  }
+
+  validateForm() {
+
+    let fields = this.state.fields;
+    let errors = {};
+    let formIsValid = true;
+
+    if (!fields["login"]) {
+      formIsValid = false;
+      errors["login"] = "To pole jest wymagane. Proszę podać swój login!";
+    }
+    if (!fields["password"]) {
+      formIsValid = false;
+      errors["password"] = "To pole jest wymagane. Proszę podać swoje hasło!";
+    }
+
+    this.setState({
+      errors: errors
+    });
+    return formIsValid;
+  }
 
 
-  render(){
+  render() {
 
-        return (
-          <form>
-          <div className="register--container">
-            <span className="error--info"></span>
+    return (
+      <form name="userLoginForm" onSubmit={this.submituserLoginForm} >
+        <div className="login--container">
+          <span className="error--info"></span>
+          <br />
+          <label for="login" id="loginLabel"> Login
+            <br />
+            <input type="text" name="login" id="login" value={this.state.fields.login} onChange={this.handleChange} />
+          </label>
+          <div className="errorMessage">{this.state.errors.login}</div>
+          <label for="password" id="passwordLabel"> Hasło
             <br/>
-            <label for="login" id="loginLabel"> Login
-            <br/>
-                <input type="text" id="login" name="login" required/>
-            </label>
-            <label for="password" id="passwordLabel"> Hasło
-                <input type="password" id="password" name="password" required />
-            </label>
-            <br/>
-            <input type="submit" id="submit" value="Zaloguj" />
+                <input type="password" name="password" id="password" value={this.state.fields.password} onChange={this.handleChange} />
+          </label>
+          <div className="errorMessage">{this.state.errors.password}</div>
+          <br />
+          <input type="submit" id="submit" value="Zaloguj" />
         </div>
       </form>
     );
   }
 }
-
 
 export default LoginForm;
