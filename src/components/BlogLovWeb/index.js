fbdbb415041672bf8228265973a49cc461da0e72
Maciej Matuszczak <maciej.matuszczak96@gmail.com>
2019-01-14 19:48:26 +0100

    [connect_redux_to_rest_components] Added actions to get blog by slug and add new post

--- src/components/BlogLovWeb/index.js
+++ src/components/BlogLovWeb/index.js
@@ -1,21 +1,37 @@
 import React, {Component} from 'react';
 import Entry from '../Entry';
 import './style.css';
+import { connect } from 'react-redux';
+import { fetch_blog_by_slug } from '../../actions/blogActions';
 import header from '../../assets/images/weddingheader.png';
 
 import { mock_entries } from '../../mocks';
 
 
 class BlogLovWeb extends Component{
+
+    componentDidMount(){
+        if(!this.props.blogs){
+            this.props.fetch_blog_by_slug(this.props.match.params.slug)
+        }
+    }
+
     render(){
+        const entries = this.props.blogs.filter(blog => blog.slug === this.props.match.params.slug);
+        console.log(entries[0])
         return(
             <div className="BlogLovWeb--container">
                 <div className="BlogLovWeb--header">
                     <img src={header} alt="header" className="BlogLovWeb--header-img"/>
                 </div>
-                <div className="BlogLovWeb--content-container">
+                <div className="BlogLovWeb--content-container"></div>
 
-                {mock_entries.map(entry => <Entry key={entry.title} entry={entry}/>)}
+                {
+                    (entries[0].entries.length > 0)?
+                    entries[0].entries.map(entry => <Entry key={entry.title} entry={entry}/>):
+                    mock_entries.map(entry => <Entry key={entry.title} entry={entry}/>)
+
+                }
 
                 <div className="BlogLovWeb--footer">
                 <p>Aliquip commodo id id incididunt ad in eiusmod. Consectetur culpa esse proident adipisicing est 
@@ -28,4 +44,17 @@ class BlogLovWeb extends Component{
         );
     }
 }
-export default BlogLovWeb;
+
+const mapStateToProps = (state) => {
+    return {
+        blogs: state.blog.blogs
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        fetch_blog: (slug) => dispatch(fetch_blog_by_slug(slug))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BlogLovWeb);
--- src/reducers/blogReducer.js
+++ src/reducers/blogReducer.js
@@ -1,7 +1,21 @@
-import { FETCHING_BLOGS, FETCHING_BLOGS_SUCCESS, FETCHING_BLOGS_FAIL, CREATE_BLOG, CREATE_BLOG_SUCCESS, CREATE_BLOG_FAIL } from '../actions/blogActions';
+import { 
+    FETCHING_BLOGS,
+    FETCHING_BLOGS_SUCCESS,
+    FETCHING_BLOGS_FAIL,
+    CREATE_BLOG,
+    CREATE_BLOG_SUCCESS,
+    CREATE_BLOG_FAIL,
+    FETCHING_BLOG_BY_SLUG,
+    FETCHING_BLOG_BY_SLUG_SUCCESS,
+    FETCHING_BLOG_BY_SLUG_FAIL,
+    ADD_POST,
+    ADD_POST_SUCCESS,
+    ADD_POST_FAIL,
+} from '../actions/blogActions';
 
 const initialState = {
     blogs: [],
+    entries: [],
     fetching: false,
     error: null
 }
@@ -37,6 +51,42 @@ export const reducer = (state=initialState, action) => {
                 fetching: false
             };
         case CREATE_BLOG_FAIL:
+            return {
+                ...state,
+                fetching: false,
+                error: action.payload
+            };
+        case FETCHING_BLOG_BY_SLUG:
+            return {
+                ...state,
+                fetching: true
+            };
+        case FETCHING_BLOG_BY_SLUG_SUCCESS:
+            return {
+                ...state,
+                blogs: [...state, action.payload.data],
+                fetching: false,
+                error: null
+            };
+        case FETCHING_BLOG_BY_SLUG_FAIL:
+            return {
+                ...state,
+                fetching: false,
+                error: action.payload
+            };
+        case ADD_POST:
+            return {
+                ...state,
+                fetching: true
+            };
+        case ADD_POST_SUCCESS:
+            return {
+                ...state,
+                blogs: [...state.blogs, action.payload.data],
+                fetching: false,
+                error: null
+            };
+        case ADD_POST_FAIL:
             return {
                 ...state,
                 fetching: false,
